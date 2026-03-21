58ae11fa18db9a3523608af19e30bf343c12b77a
kundan <kundan8789@gmail.com>
2026-03-21 22:03:43 +0530

    start creating Auth Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div>
-      <h1>Welcome to the App</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/auth' element={<Auth/>} />
+    </Routes>
   )
 }
 
